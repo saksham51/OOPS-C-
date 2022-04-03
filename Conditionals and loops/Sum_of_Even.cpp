b4985b03f41affb6c3de27d8c5dcd27fa371829a
saksham51 <93071449+saksham51@users.noreply.github.com>
2022-04-03 16:03:37 +0530

    Sum_of_Even.cpp

---
+++ Conditionals and loops/Sum_of_Even.cpp
@@ -0,0 +1,34 @@
+/*Given a number N, print sum of all even numbers from 1 to N.
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12      *\
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+	Pre-requisite to solve this problem:
+	In this question, we have to find sum of all even numbers till n. To take sum of all the even numbers, we have to take a variable and initialize it to zero. Let us name this variable as sum. 
+	Following code is used to add 2 to sum variable and update it to the same sum variable:
+	sum = sum + 2 
+	
+	Hint to solve this problem: We will have to loop on even numbers only and add the even numbers to the same sum variable, described above.
+*/  
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n,i;
+    int sum=0;
+    cin>>n;
+    for(i=0;i<=n;i=i+2)
+    {
+        sum=sum+i;
+    }
+    cout<<sum;
+}
