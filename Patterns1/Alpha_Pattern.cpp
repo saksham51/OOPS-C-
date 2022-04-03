f67d83b04c8aa09c652cc48830a66739f1a72b48
saksham51 <93071449+saksham51@users.noreply.github.com>
2022-04-03 22:19:37 +0530

    Alpha_Pattern.cpp

---
+++ Patterns1/Alpha_Pattern.cpp
@@ -0,0 +1,54 @@
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 3
+ A
+ BB
+ CCC
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+7
+Sample Output 1:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+Sample Input 2:
+6
+Sample Output 2:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF */
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int i,j,n;
+    char c;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            c='A'+i-1;
+            cout<<c;
+        }
+        cout<<endl;
+    }
+  
+}
